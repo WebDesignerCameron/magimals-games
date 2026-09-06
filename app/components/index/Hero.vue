ed4b7622e8ce5f229c3ce3566d1d98caf00f02b5
WebDesignerCameron <webdesignercameron@gmail.com>
2026-09-06 16:31:22 +0100

    Add hero component

---
+++ app/components/index/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="hero">
+        <h2>Magimals Games</h2>
+        <p>
+            This website is a Magimals launcher for
+            different games made by the Magimals
+            company.
+        </p>
+    </section>
+</template>
