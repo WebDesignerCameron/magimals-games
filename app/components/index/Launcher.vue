9f83d4582d98b6fcd0b53192ae80fead3e22f128
WebDesignerCameron <webdesignercameron@gmail.com>
2026-09-11 16:17:56 +0100

    Elaborate

--- app/components/index/Launcher.vue
+++ app/components/index/Launcher.vue
@@ -3,6 +3,6 @@
         <h2>
             Games
         </h2>
-        No games available currently.
+        No games available currently(still migrating code from the <code>src</code> repository).
     </section>
 </template>
