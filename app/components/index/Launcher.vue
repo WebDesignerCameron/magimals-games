10b9beccd6b91df386d35c2265c9bb8f0fdbc75b
WebDesignerCameron <webdesignercameron@gmail.com>
2026-09-11 16:24:37 +0100

    Format Launcher.vue

--- app/components/index/Launcher.vue
+++ app/components/index/Launcher.vue
@@ -3,6 +3,8 @@
         <h2>
             Games
         </h2>
-        No games available currently(still migrating code from the <code>src</code> repository).
+        <br>
+        No games available currently(still migrating code
+        from the <code>src</code> repository).
     </section>
 </template>
