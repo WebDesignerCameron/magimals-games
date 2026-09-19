d8b3056265034184430579eb85a59318600b2e2d
Cameron <webdesignercameron@gmail.com>
2026-09-19 13:10:45 +0100

    Add link to Toxic page in Launcher component

--- app/components/index/Launcher.vue
+++ app/components/index/Launcher.vue
@@ -8,6 +8,7 @@
             <h3>Magimals Toxic</h3>
             <IndexWarning />
             Version: Beta 1.0 Snapshot 1 TSB
+            Link: <a href="/toxic.html">Here</a>
         </article>
     </section>
 </template>
